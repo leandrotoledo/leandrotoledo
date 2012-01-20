b3859af2b6165f96562d3e3222eb4821c9e1bc26
Leandro Toledo de Souza <leandrotoledodesouza@gmail.com>
2012-01-20 12:42:29 -0200

    CSS compression/Google Analytics column

--- src/core/models.py
+++ src/core/models.py
@@ -59,6 +59,7 @@ class UserProfile(models.Model):
 
 
 class SiteDetail(models.Model):
-    site = models.OneToOneField(Site)
-    keywords = models.CharField(max_length=150)
-    description = models.CharField(max_length=150)
+	site = models.OneToOneField(Site)
+	keywords = models.CharField(max_length=150)
+	description = models.CharField(max_length=150)
+	analytics_account_id = models.CharField(max_length=50)
