2758c9d5ec45c68f7bb8f0d940c3ad03ec1caed0
Leandro Toledo <leandrotoledodesouza@gmail.com>
2012-01-29 09:32:22 -0200

    Fixing

--- src/core/feeds.py
+++ src/core/feeds.py
@@ -3,15 +3,15 @@ from django.contrib.sites.models import Site
 from core.models import Post
 
 class PostsFeed(Feed):
-	title = Site.objects.get_current().name
-	link = '/'
-	description = Site.objects.get_current().sitedetail.description
-	
-	def items(self):
-		return Post.objects.filter(is_draft=False)
-		
-	def item_title(self, item):
-		return item.title
-	
-	def item_description(self, item):
-		return item.excerpt
+    title = Site.objects.get_current().name
+    link = '/'
+    description = '' # Site.objects.get_current().sitedetail.description
+
+    def items(self):
+        return Post.objects.filter(is_draft=False)
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.excerpt
