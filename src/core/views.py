ea8cc0b7bc94e2fdb0940238d8caba50e2498efe
Leandro Toledo de Souza <leandrotoledodesouza@gmail.com>
2012-02-01 08:20:45 -0200

    Categorias e Arquivos Mensais

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView, RedirectView
 from django.views.generic.dates import MonthArchiveView
@@ -19,7 +20,26 @@ class PostDetailView(DetailView):
 class PostMonthArchiveView(MonthArchiveView):
     model = Post
     date_field = 'published_date'
-    month_format= '%m'
+    paginate_by = 4
+    month_format = '%m'
+    template_name = 'core/post_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostMonthArchiveView, self).get_context_data(**kwargs)
+        messages.info(self.request, 'Arquivos Mensais: <em>%s</em>' % self.request.user)
+        return context
 
 class CategoriesListView(ListView):
     model = Category
+    paginate_by = 4
+    template_name = 'core/post_list.html'
+
+    def get_queryset(self):
+        category = self.kwargs['category']
+        self.queryset = Post.objects.filter(category__slug=category)
+        return self.queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoriesListView, self).get_context_data(**kwargs)
+        messages.info(self.request, 'Arquivos da Categoria: <em>%s</em>' % self.request.user)
+        return context
